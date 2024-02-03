a746a3c6139244eaba5bc0198c3b2f4249c10db4
Meow Roar <sareacct91@gmail.com>
2024-02-03 15:14:00 -0800

    added loginValidate mw

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const { Traveller } = require("../models");
+const auth = require('basic-auth');
+const bcrypt = require('bcrypt');
+const { UnauthorizedError } = require("../errors");
+
+
+async function loginValidate(req,res,next) {
+
+  const { name, pass } = auth(req);
+  console.log(name, '\n', pass);
+
+  const { password } = await Traveller.findOne({
+    where: {
+      email: name
+    }
+  })
+
+  if (!password) {
+    throw new UnauthorizedError('Bad credential');
+  }
+
+  if (await bcrypt.compare(pass, password)) {
+    next();
+  } else {
+    throw new UnauthorizedError('Bad credential');
+  }
+}
+
+module.exports = loginValidate;
